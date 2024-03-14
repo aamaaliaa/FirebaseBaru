96d317ce4414c7193456c5d83683bdf5a6c6f705
aamaaliaa <priliantiniamalia@gmail.com>
2024-03-14 21:32:05 +0700

    FirebaseBaru
    Praktikum PBB 2 Tugas 2
    
    Co-Authored-By: aamaaliaa <161192697+aamaaliaa@users.noreply.github.com>

--- app/src/main/java/com/example/firebasebaru/MainActivity.kt
+++ app/src/main/java/com/example/firebasebaru/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.example.firebasebaru
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
+import com.example.firebasebaru.databinding.ActivityMainBinding
+import com.firebase.ui.auth.AuthUI
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import org.w3c.dom.Text
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private var auth: FirebaseAuth? = null
+    private val RC_SIGN_IN = 1
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //inisialisasi ID (Button)
+        binding.logout.setOnClickListener(this)
+        binding.save.setOnClickListener(this)
+        binding.showdata.setOnClickListener(this)
+        //Mendapatkan Instance Firebase Autentfikasi
+        auth = FirebaseAuth.getInstance()
+    }
+    private fun isEmpty(s: String):Boolean {
+        return TextUtils.isEmpty(s)
+    }
+    override fun onClick(p0: View?) {
+        when (p0?.getId()) {
+            R.id.save -> {
+                // statment program untuk simpan data
+            }
+            R.id.logout ->
+                // Statement program untuk lohout/keluar
+                AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnCompleteListener(object :
+                        OnCompleteListener<Void> {
+                        override fun onComplete(p0: Task<Void>) {
+                            Toast.makeText(this@MainActivity,"Logut Berhasil", Toast.LENGTH_SHORT).show()
+                            intent = Intent (applicationContext, LoginActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                        })
+            R.id.showdata -> {
+            }
+        }
     }
 }
